7179ee8108407a27c07d461c1eee78620f17957c
Aladix002 <xbotlo01@stud.fit.vut.cz>
2025-03-12 15:44:40 +0100

    uprava sipok

--- cpp_project_template/src/main.cpp
+++ cpp_project_template/src/main.cpp
@@ -65,18 +65,18 @@ int main(int argc, char* argv[]) {
         int key = get_key();
         if (key == 27) {  // ESC key to exit
             break;
-        } else if (key == 65) {  // Šípka hore
+        } else if (key == 38) {  // Šípka hore
             motor_speeds[0] += 10;
             motor_speeds[1] += 10;
             motor_controller->set_motor_speeds({static_cast<float>(motor_speeds[0]), static_cast<float>(motor_speeds[1])});
-        } else if (key == 66) {  // Šípka dole
+        } else if (key == 40) {  // Šípka dole
             motor_speeds[0] -= 10;
             motor_speeds[1] -= 10;
             motor_controller->set_motor_speeds({static_cast<float>(motor_speeds[0]), static_cast<float>(motor_speeds[1])});
-        } else if (key == 67) {  // Šípka vpravo
+        } else if (key == 39) {  // Šípka vpravo
             motor_speeds[0] += 5;
             motor_controller->set_motor_speeds({static_cast<float>(motor_speeds[0]), static_cast<float>(motor_speeds[1])});
-        } else if (key == 68) {  // Šípka vlavo
+        } else if (key == 37) {  // Šípka vlavo
             motor_speeds[1] += 5;
             motor_controller->set_motor_speeds({static_cast<float>(motor_speeds[0]), static_cast<float>(motor_speeds[1])});
         }
