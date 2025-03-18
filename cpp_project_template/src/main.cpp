8f318a5f7f3cbf3f385a64be1c377fb09a5859d0
Aladix002 <xbotlo01@stud.fit.vut.cz>
2025-03-18 16:31:09 +0100

    main

--- cpp_project_template/src/main.cpp
+++ cpp_project_template/src/main.cpp
@@ -1,44 +1,34 @@
 #include "nodes/motor.hpp"
-#include "nodes/kinematics_odometry.hpp"
 #include <thread>
 #include <chrono>
-#include <iostream>
+#include <termios.h>
+#include <unistd.h>
 
 using namespace nodes;
-using namespace algorithms;
-
 int main(int argc, char* argv[]) {
     rclcpp::init(argc, argv);
-
-    double wheel_radius = 0.03288;
-    double wheel_base = 0.12834;
-    int ticks_revolution = 576;
-
     auto motor_controller = std::make_shared<MotorController>();
-    auto kinematics_odometry = std::make_shared<KinematicsOdometry>(wheel_radius, wheel_base, ticks_revolution);
-
     auto executor = std::make_shared<rclcpp::executors::MultiThreadedExecutor>();
     executor->add_node(motor_controller);
-    executor->add_node(kinematics_odometry);
 
     std::thread executor_thread([executor]() { executor->spin(); });
+    std::vector<uint8_t> motor_speeds = {0, 0}; // Počiatočné rýchlosti motorov
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    while (rclcpp::ok()) {
 
-    std::vector<uint8_t> motor_speeds = {0, 0};
-    motor_speeds[0] = 170;
-    motor_speeds[1] = 165;
 
-    motor_controller->set_motor_speeds({static_cast<float>(motor_speeds[0]), static_cast<float>(motor_speeds[1])});
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+        motor_speeds[0] = 140;
+        motor_speeds[1] = 140;
 
-    algorithms::Coordinates position = kinematics_odometry->getPosition();
+        // Nastaví rýchlosť motorov
+        motor_controller->set_motor_speeds({static_cast<float>(motor_speeds[0]), static_cast<float>(motor_speeds[1])});
 
-    std::cout << "Current Position: X = " << position.x << ", Y = " << position.y << std::endl;
+
+
+    }
 
     rclcpp::shutdown();
     executor_thread.join();
-
     return 0;
 }
