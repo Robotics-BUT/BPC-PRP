292ce7b12b6072375e854a660c4d0712326a4adc
Aladix002 <xbotlo01@stud.fit.vut.cz>
2025-03-19 17:37:56 +0100

    8 ide latest version

--- cpp_project_template/src/nodes/line.cpp
+++ cpp_project_template/src/nodes/line.cpp
@@ -3,7 +3,7 @@
 
 using namespace nodes;
 
-LineSensorListener::LineSensorListener() : Node("line_sensor_listener"), Kp(0.01), Kd(0.005), base_speed(130), last_error(0.0)
+LineSensorListener::LineSensorListener() : Node("line_sensor_listener"), Kp(0.014), Kd(0.004), base_speed(132), last_error(0.0)
 {
     line_sensors_subscriber_ = this->create_subscription<std_msgs::msg::UInt16MultiArray>(
         "/bpc_prp_robot/line_sensors", 10,
@@ -24,8 +24,21 @@ void LineSensorListener::on_line_sensors_msg(std::shared_ptr<std_msgs::msg::UInt
         float error = left_value - right_value;
         float d_error = error - last_error;
 
-        float left_motor_speed = base_speed - (Kp * error + Kd * d_error);
-        float right_motor_speed = base_speed + (Kp * error + Kd * d_error);
+        float right_motor_speed;
+        float left_motor_speed;
+
+        if (error > 0 ) {
+             right_motor_speed = base_speed;
+             left_motor_speed = base_speed - (Kp * error + Kd * d_error);
+        }
+        else if (error < 0) {
+             right_motor_speed = base_speed + (Kp * error + Kd * d_error);
+             left_motor_speed = base_speed;
+        }
+        else {
+             right_motor_speed = base_speed;
+             left_motor_speed = base_speed ;
+        }
 
         motor_controller_->set_motor_speeds({
             std::clamp(left_motor_speed, 0.0f, 255.0f),
